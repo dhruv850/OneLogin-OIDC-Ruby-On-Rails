e776fed07b2b2db1232efc354b9ce03e0eb9a1d1
Dhruv Patel <dhruv850.dp@gmail.com>
2021-01-20 01:03:17 -0500

    Ruby on Rails OneLogin OIDC

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+    before_action :require_current_user
+  
+    def index
+    end
+end
+  
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'login', to: 'sessions#new', as: 'new_session'
+  get 'oauth/callback', to: 'sessions#callback', as: 'session_callback'
+  get 'logout', to: 'sessions#destroy', as: 'destroy_session'
+
+  get 'dashboard', to: 'dashboard#index'
+
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
